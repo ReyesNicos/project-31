2c507ad1d3df5ef3cbe73b0573a5885016d8e93d
ReyesNicos <75365528+ReyesNicos@users.noreply.github.com>
2021-06-05 18:43:11 +0800

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,31 @@
+class particle {
+    constructor(x,y){
+    
+      var options={
+      isStatic:false,
+      restitution:0.3,
+      friction:0.5,
+      density:1.2,
+    }
+    this.r=10
+    this.body= Bodies.circle(x,y,10,options);
+    this.color=color(random(0, 255), random(0.255), random(0,255));
+
+    World.add(world,this.body);
+    
+    }
+    display(){
+    
+    var pos=this.body.position;
+    var angle= this.body.angle;
+    push ();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    strokeWeight(4);
+    stroke("black");
+    fill(this.color);
+    ellipseMode(RADIUS);
+    ellipse(0,0,this.r,this.r);
+    pop();
+    }
+}
